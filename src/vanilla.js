8788a986022dd91353bf7774fd722758e70b3e9a
mjanssen <dev.marnix@gmail.com>
2018-01-31 19:53:26 +0100

    Added type checking for function

--- src/vanilla.js
+++ src/vanilla.js
@@ -1,22 +1,23 @@
 import store from './store';
+import { get } from './fetch';
 
-export default _apiData => target => {
+export default _apiData => _target => {
 
-  Object.keys(_apiData).map(k => target.prototype[k] = {});
+  Object.keys(_apiData).map(k => _target.prototype[k] = {});
 
   Object.entries(_apiData).map(([k, url]) => {
     if (typeof store[url] === 'undefined') {
       return get(url).then(result => {
-        target.prototype[k] = store[url] = result;
-        target.prototype.didRecieveData(store);
+        _target.prototype[k] = store[url] = result;
+        if (typeof _target.prototype.didRecieveData === 'function') {
+          _target.prototype.didRecieveData(store);
+        }
       });
     }
 
-    target.prototype[k] = store[url];
-    target.prototype.didRecieveData(store);
+    _target.prototype[k] = store[url];
+    if (typeof _target.prototype.didRecieveData === 'function') {
+      _target.prototype.didRecieveData(store);
+    }
   })
-
-  if (typeof target.prototype.didRecieveData === 'function') {
-    target.prototype.didRecieveData(store);
-  }
 }
