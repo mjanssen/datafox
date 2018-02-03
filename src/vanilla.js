35b04f676b294a45b479452f265c8f1246b6089d
mjanssen <dev.marnix@gmail.com>
2018-02-03 12:13:15 +0100

    Return target class

--- src/vanilla.js
+++ src/vanilla.js
@@ -1,5 +1,5 @@
-import store from "./store";
-import { get } from "./fetch";
+import store from './store';
+import { get } from './fetch';
 
 export default _apiData => _target => {
   Object.keys(_apiData).map(key => {
@@ -11,12 +11,14 @@ export default _apiData => _target => {
     get(_apiData[key]).then(result => {
       store.set(key, result);
       _target.prototype[key] = result;
-      if (typeof _target.prototype.didRecieveData === "function") {
-        _target.prototype.didRecieveData();
+      if (typeof _target.prototype.didReceiveData === 'function') {
+        _target.prototype.didReceiveData();
       }
     });
   });
-  if (typeof _target.prototype.didRecieveData === "function") {
-    _target.prototype.didRecieveData();
+  if (typeof _target.prototype.didReceiveData === 'function') {
+    _target.prototype.didReceiveData();
   }
+
+  return _target;
 };
